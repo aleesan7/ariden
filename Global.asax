667b1352acd54a80d48d943cd7a9806bb15f0f3c
aleesan7 <ka_sanchezsamayoa@outlook.com>
2016-04-21 17:45:52 -0600

    Added files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ARIDEN.Global" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ARIDEN.Startup))]
+namespace ARIDEN
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
